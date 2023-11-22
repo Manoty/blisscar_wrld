838e1e59d3aad060b758ad519e1e7a877b9bba68
Manoty <kevinnoty21@gmail.com>
2023-11-22 14:28:52 +0300

    initial commit creating the register page

---
+++ auth/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views as my_views
+
+
+urlpatterns = [
+    path('register/', my_views.register, name='registration-url'),
+
+]
---
+++ authentic/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib import messages
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User Registered Succesfuly')
+            return redirect('registration-url')
+    else:
+        form = RegistrationForm()
+
+
+    return render(request, 'aunthentic/reegister.html', {"form":form})
+
